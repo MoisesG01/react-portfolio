c6b52e1c02aa6c5ec64f2ab155da4d1936fd3921
Moises Gonçalves <moises.goncalves@fatec.sp.gov.br>
2025-06-19 22:09:57 -0300

    Add header section with image, text, and social links

---
+++ src/sections/header/data.js
@@ -0,0 +1,15 @@
+import {
+  AiFillGithub,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+
+const data = [
+  { id: 1, link: "https://instagram.com", icon: <AiOutlineInstagram /> },
+  { id: 2, link: "https://twitter.com", icon: <AiOutlineTwitter /> },
+  { id: 3, link: "https://linkedin.com", icon: <AiOutlineLinkedin /> },
+  { id: 4, link: "https://github.com", icon: <AiFillGithub /> },
+];
+
+export default data;
