eed01ae08b19a8a1b9fe08824654ad751c20d227
Moises Gonçalves <moises.goncalves@fatec.sp.gov.br>
2025-07-01 21:23:08 -0300

    feat: implement portfolio section with category filtering and responsive design

--- src/sections/portfolio/ProjectsCategories.jsx
+++ src/sections/portfolio/ProjectsCategories.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState } from 'react'
+import CategoryButton from './CategoryButton'
 
-const ProjectsCategories = () => {
-  return <div>ProjectsCategories</div>;
-};
+const ProjectsCategories = ({categories, onFilterProjects}) => {
+    const [activeCategory, setActiveCategory] = useState('all');
 
-export default ProjectsCategories;
+    const changeCategoryHandler = (activeCat) => {
+        setActiveCategory(activeCat);
+        onFilterProjects(activeCat);
+    }
+
+  return (
+    <div className='portfolio__categories'>
+        {
+            categories.map(category => (
+                <CategoryButton key={category} category={category} onChangeCategory={() => changeCategoryHandler(category)} className={`btn cat__btn ${activeCategory == category ? 'primary' : 'white'}`}/>
+            ))
+        }
+    </div>
+  )
+}
+
+export default ProjectsCategories
